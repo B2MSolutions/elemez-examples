7c1c75c792b793d824f038c5dadbf6c923186dc4
youhana-hana <meyouhana@gmail.com>
2014-11-26 12:06:15 +0000

    create an example for Battery Cycle Count

---
+++ ce/BatteryCycleCount/Main.cs
@@ -0,0 +1,26 @@
+﻿namespace BatteryCycleCount
+{
+    using System;
+    using System.Windows.Forms;
+    using Microsoft.Win32;
+
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        
+        private void Save_Click(object sender, EventArgs e)
+        {
+            int cycleCount = int.Parse(cycleCountTextBox.Text);
+
+            using (RegistryKey subKey = Registry.LocalMachine.CreateSubKey(@"Software\elemezIntegration\"))
+            {
+                subKey.SetValue("battery_charge_cycle_count", cycleCount, RegistryValueKind.DWord);
+            }
+
+            MessageBox.Show("Saved");
+        }
+    }
+}
